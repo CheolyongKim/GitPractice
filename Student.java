cc7c8b21a32a9a675dcc922afd17d788844bc3eb
cyk-main <cyk.main@gmail.com>
2026-05-10 20:44:40 +0900

    Add Student Class a Constructor and a method printHello

--- Student.java
+++ Student.java
@@ -1,4 +1,13 @@
 public class Student {
     private int id;
     private String name;
+
+    public Student(int id, String name){
+        this.id = 10*id;
+        this.name=name;
+    }
+
+    private void printHello(){
+        System.out.println("Hello!");
+    }
 }
